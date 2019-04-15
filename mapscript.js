b1f8e39f1222b1487c03fe7e175e7c76d18c51bc
Priyanka Priyanka <743897@student.tor.loc>
2019-04-15 18:19:07 -0400

    commit3

--- mapscript.js
+++ mapscript.js
@@ -1,19 +1,11 @@
-var map1,map2,map3;
+var map;
         function initialize(){
-            var mapOptions1={
-                zoom:2,
-                center:{lat:40.758904, lng:-73.985128}
+            var mapOptions={
+                zoom:12,
+                center:new google.maps.LatLng(40.758904, -73.985128)
             };
-            var mapOptions2={
-                zoom:10,
-                center:{lat:40.758904, lng:-73.985128}  
-            };
-            var mapOptions3={
-                zoom:18,
-                center:{lat:40.758904, lng:-73.985128}  
-            };
-            map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
-            map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOptions2);
-            map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOptions3);
+           
+            map=new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
+           
         }
         google.maps.event.addDomListener(window,'load',initialize);
